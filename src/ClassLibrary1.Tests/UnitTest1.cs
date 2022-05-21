82237403bbc2ea5be3b08386f45eac821075f4cc
Jithin John <jithin.john@live.com>
2022-05-21 17:01:55 +0530

    Add files

---
+++ src/ClassLibrary1.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace ClassLibrary1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void VerifyIfFirstBrokerCanBeRetrieved()
+        {
+
+        }
+
+        [Fact]
+        public void VerifyIfBrokerNameCanBeFiltered()
+        {
+
+        }
+    }
+}
---
+++ src/ClassLibrary1/Class1.cs
@@ -0,0 +1,34 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private readonly IList<string> _brokers =
+            new List<string>{
+                                    "Vangard",
+                                    "Ameritrade",
+                                    "Capital",
+                                    "City"
+                                };
+        public string GetFirstBroker()
+        {
+            return null;
+        }
+
+        public Task<string> GetFilteredBroker(string searchTag)
+        {
+            LongRunningTask1();
+            LongRunningTask1();
+            return null;
+        }
+
+        private async Task LongRunningTask1()
+        {
+            await Task.Delay(1000);
+        }
+
+        private async Task LongRunningTask2()
+        {
+            await Task.Delay(1000);
+        }
+    }
+}
